a7876ae7b35088afb451a0fb77285f3086cd01d2
jmfuselier <142937011+jmfuselier@users.noreply.github.com>
2025-04-28 09:27:43 -0500

    Update shiny_vector_filter_logical.R

--- R/shiny_vector_filter_logical.R
+++ R/shiny_vector_filter_logical.R
@@ -31,7 +31,8 @@ shiny_vector_filter.logical <- function(data, inputId, ...) {
             0.75s ease-out 0s 1 shinyDataFilterEnlargeX, 
             0.5s ease-in  0s 1 shinyDataFilterFadeIn; 
           transform-origin: left;",
-        #  shiny::plotOutput(ns("plot"), height = "100%")),
+        #  shiny::plotOutput(ns("plot"), height = "100%")
+          ),
         shiny::checkboxGroupInput(ns("param"), NULL,
           choices = choices(),
        #   selected = shiny::isolate(input$param) %||% c(),
