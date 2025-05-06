c55483a961ffcc4d2fd6e87d96e3ba3dc3376987
jmfuselier <142937011+jmfuselier@users.noreply.github.com>
2025-05-06 12:12:07 -0500

    Update shiny_data_filter_item.R

--- R/shiny_data_filter_item.R
+++ R/shiny_data_filter_item.R
@@ -99,6 +99,7 @@ shiny_data_filter_item <- function(input, output, session, data,
   column_filter_ui <- shiny::tagList(
     shiny::h4(
       fillRow(
+        flex = NA,
         shiny::icon("grip-vertical", class = "sortableJS-handle"),
         shiny::uiOutput(ns("column_name"), inline = TRUE),
         shiny::actionLink(
